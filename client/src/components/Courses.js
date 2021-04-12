b5c0c35a8e24eed362c0a28aa2cae369c8043227
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-13 01:58:47 +0200

    Fix fetch infinite loop And clean up markup

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -4,37 +4,25 @@ import axios from "axios";
 export default function Courses() {
 
     // Add hooks to manage state of component
-    const [courses, setCourses] = useState([])
+    const [courses, getCourses] = useState([])
 
     // Fetch from API
     useEffect( () => {
+        getAllCourses()
+    }, [])
+
+    const getAllCourses = () => {
         axios.get(`http://localhost:5000/api/courses`)
-            .then(response => setCourses(response))
+            .then(response => getCourses(response.data))
             .catch(error => console.log('Error fetching and parsing data', error))
-    }, [courses])
+    }
 
     console.log(courses)
 
+
     return (
         <div className="wrap main--grid">
-            <a className="course--module course--link" href="course-detail.html">
-                <h2 className="course--label">Course</h2>
-                <h3 className="course--title">Build a Basic Bookcase</h3>
-            </a>
-            <a className="course--module course--link" href="course-detail.html">
-                <h2 className="course--label">Course</h2>
-                <h3 className="course--title">Learn How to Program</h3>
-            </a>
-            <a className="course--module course--link" href="course-detail.html">
-                <h2 className="course--label">Course</h2>
-                <h3 className="course--title">Learn How to Test Programs</h3>
-            </a>
-            <a className="course--module course--add--module" href="create-course.html">
-          <span className="course--add--title">
-            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 " /></svg>
-            New Course
-          </span>
-            </a>
+
         </div>
     )
 }
