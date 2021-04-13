507e1de97e4a38f3ac71acdc3dbd8615948143c6
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-14 01:26:50 +0200

    Render initial draft with simple to retrieve data from response

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -30,14 +30,59 @@ export default function CourseDetail() {
     }, [query])
 
     console.log(data)
-
     console.log(performSearch)
+
+    const title = data.title
+    const description = data.description
+    const estimatedTime = data.estimatedTime
+    const materialsNeeded = data.materialsNeeded
+    const firstName = data.firstName
+    const lastName = data.lastName
+
+    console.log(firstName)
+    console.log(lastName)
 
     // Listener for changes of location
 
     return (
-        <div className="container">
-
-        </div>
+        <main>
+            <div className="actions--bar">
+                <div className="wrap">
+                    <a className="button" href="update-course.html">Update Course</a>
+                    <a className="button" href="#">Delete Course</a>
+                    <a className="button button-secondary" href="index.html">Return to List</a>
+                </div>
+            </div>
+            <div className="wrap">
+                <h2>Course Detail</h2>
+                <form>
+                    <div className="main--flex">
+                        <div>
+                            <h3 className="course--detail--title">Course</h3>
+                            <h4 className="course--name">{title}</h4>
+                            <p>{`By ${firstName} ${lastName}`}</p>
+                            <p>{description}</p>
+                        </div>
+                        <div>
+                            <h3 className="course--detail--title">Estimated Time</h3>
+                            <p>{estimatedTime}</p>
+                            <h3 className="course--detail--title">Materials Needed</h3>
+                            <ul className="course--detail--list">
+                                <li>1/2 x 3/4 inch parting strip</li>
+                                <li>1 x 2 common pine</li>
+                                <li>1 x 4 common pine</li>
+                                <li>1 x 10 common pine</li>
+                                <li>1/4 inch thick lauan plywood</li>
+                                <li>Finishing Nails</li>
+                                <li>Sandpaper</li>
+                                <li>Wood Glue</li>
+                                <li>Wood Filler</li>
+                                <li>Minwax Oil Based Polyurethane</li>
+                            </ul>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </main>
     );
 }
