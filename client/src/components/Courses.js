dd09e2cc2b1e701facc62cf5753c4ae78f4d45fc
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-14 22:21:43 +0200

    Simplify Courses component

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -10,23 +10,19 @@ export default function Courses() {
 
     // Fetch from API
     useEffect( () => {
-        getAllCourses()
-    }, [])
-
-
-    const getAllCourses = () => {
         axios.get(`http://localhost:5000/api/courses`)
             .then(response => getCourses(response.data))
             .catch(error => console.log('Error fetching and parsing data', error))
-    }
-
+    }, [])
 
     console.log(courses)
 
-    courseContainer = courses.map(courses => <a className="course--module course--link" key={courses.id}>
+    courseContainer = courses.map(courses =>
+        <a className="course--module course--link" key={courses.id}>
             <h2 className="course--label">Course</h2>
             <h3 className="course--title">{courses.title}</h3>
-    </a>)
+        </a>
+    )
 
     return (
         <div className="wrap main--grid">
