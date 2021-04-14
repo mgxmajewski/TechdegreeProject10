6bbcf078b7c0369c30a77c98514fe24e849ceafb
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-14 23:32:59 +0200

    Change message for user in case there is no such a course

--- client/src/components/NoDetails.js
+++ client/src/components/NoDetails.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const NoDetails = () => (
     <div>
-        <h3>Sorry, no details to show.</h3>
+        <h3>Sorry, no course with this ID</h3>
     </div>
 )
-
 export default NoDetails;
