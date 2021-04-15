01329cd2c862638ef58e617fe56926b9a11b5d82
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-15 02:01:29 +0200

    Handle creating new course At the moment userId is hard coded

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -1,34 +1,54 @@
 import React, { useState, useEffect } from 'react';
+import ValidationError from "./ValidationError";
 import axios from "axios";
 
 
 
 export default function CreateCourse() {
 
-    const [course, setCourse] = useState([])
+    const [course, setCourse] = useState({
+        title: "",
+        description: "",
+        estimatedTime:"",
+        materialsNeeded:"",
+        userId: 1
+    })
+    const [response, setResponse] = useState([])
 
-    // Fetch from API
-    useEffect( () => {
-        axios.post(`http://localhost:5000/api/courses/`)
-            .then(course => setCourse(course.data))
+    //https://rangle.io/blog/simplifying-controlled-inputs-with-hooks/
+
+    const submit = (e) => {
+        e.preventDefault();
+        axios.post(`http://localhost:5000/api/courses/`, {
+            title: course.title,
+            description: course.description,
+            estimatedTime: course.estimatedTime,
+            materialsNeeded: course.materialsNeeded,
+            userId: course.userId
+        })
+            .then(response => setResponse(response.data))
             .catch(error => console.log('Error fetching and parsing data', error))
-    }, [])
+    }
+
+    const handle = (e) => {
+        const newCourse = {... course}
+        newCourse[e.target.id] = e.target.value
+        setCourse(newCourse)
+        console.log(newCourse)
+    }
+
+    console.log(response)
+    console.log(course)
 
     return (
         <div className="wrap">
             <h2>Create Course</h2>
-            <div className="validation--errors">
-                <h3>Validation Errors</h3>
-                <ul>
-                    <li>Please provide a value for "Title"</li>
-                    <li>Please provide a value for "Description"</li>
-                </ul>
-            </div>
-            <form>
+            <form onSubmit={(e) => submit(e)}>
                 <div className="main--flex">
                     <div>
                         <label htmlFor="courseTitle">Course Title</label>
-                        <input id="courseTitle"
+                        <input onChange={(e) => handle(e)}
+                               id="title"
                                name="courseTitle"
                                type="text"
                                defaultValue/>
@@ -38,18 +58,21 @@ export default function CreateCourse() {
                                type="text"
                                defaultValue="Joe Smith"/>
                         <label htmlFor="courseDescription">Course Description</label>
-                        <textarea id="courseDescription"
+                        <textarea onChange={(e) => handle(e)}
+                                  id="description"
                                   name="courseDescription"
                                   defaultValue={""}/>
                     </div>
                     <div>
                         <label htmlFor="estimatedTime">Estimated Time</label>
-                        <input id="estimatedTime"
+                        <input onChange={(e) => handle(e)}
+                                id="estimatedTime"
                                name="estimatedTime"
                                type="text"
                                defaultValue/>
                         <label htmlFor="materialsNeeded">Materials Needed</label>
-                        <textarea id="materialsNeeded"
+                        <textarea onChange={(e) => handle(e)}
+                                    id="materialsNeeded"
                                   name="materialsNeeded"
                                   defaultValue={""}/>
                     </div>
