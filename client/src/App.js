ed79e92054f10f880495b16e1c5cea9069961e1d
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-15 18:57:35 +0200

    Add header to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import '../src/styles/global.css'
 import Courses from "./components/Courses";
 import CourseDetail from "./components/CourseDetail";
 import CreateCourse from "./components/CreateCourse";
+import Header from "./components/Header";
 
 import {
     BrowserRouter as Router,
@@ -12,7 +13,9 @@ import {
 
 
 const App = () => (
+
     <Router>
+    <Header/>
         <Switch>
             <Route exact path="/" component={Courses} />
             <Route path ="/courses/create" component={CreateCourse} />
