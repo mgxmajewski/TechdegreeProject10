f829f7de82fbb4e4c950b6c0abf94a7fc8f50e54
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-15 21:35:33 +0200

    Add Context with authentication component New branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,41 @@
-import './App.css';
-import '../src/styles/global.css'
-import Courses from "./components/Courses";
-import CourseDetail from "./components/CourseDetail";
-import CreateCourse from "./components/CreateCourse";
-import Header from "./components/Header";
-
+import React from 'react';
 import {
     BrowserRouter as Router,
     Route,
     Switch
-} from "react-router-dom";
+} from 'react-router-dom';
+
+import './App.css';
+import '../src/styles/global.css'
 
 
-const App = () => (
+import NotFound from './components/NotFound';
+import Courses from "./components/Courses";
+import CourseDetail from "./components/CourseDetail";
+import CreateCourse from "./components/CreateCourse";
+import Header from "./components/Header";
+import UserSignUp from './components/UserSignUp';
+import UserSignIn from './components/UserSignIn';
+import UserSignOut from './components/UserSignOut';
+import withContext from './Context';
 
+
+const HeaderWithContext = withContext(Header);
+const UserSignUpWithContext = withContext(UserSignUp);
+const UserSignInWithContext = withContext(UserSignIn);
+const UserSignOutWithContext = withContext(UserSignOut);
+
+export default () => (
     <Router>
-    <Header/>
+        <HeaderWithContext/>
         <Switch>
             <Route exact path="/" component={Courses} />
             <Route path ="/courses/create" component={CreateCourse} />
             <Route exact path ="/courses/:id" component={CourseDetail} />
+            <Route path="/signin" component={UserSignInWithContext} />
+            <Route path="/signup" component={UserSignUpWithContext} />
+            <Route path="/signout" component={UserSignOutWithContext} />
+            <Route component={NotFound} />
         </Switch>
     </Router>
 )
-
-export default App;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Header() {
-    return (
-        <header>
-            <div className="wrap header--flex">
-                <h1 className="header--logo"><a href="index.html">Courses</a></h1>
-                <nav>
-                    <ul className="header--signedout">
-                        <li><a href="sign-up.html">Sign Up</a></li>
-                        <li><a href="sign-in.html">Sign In</a></li>
-                    </ul>
-                </nav>
+export default class Header extends React.PureComponent {
+    render() {
+        const { context } = this.props;
+        const authUser = null;
+        console.log(this.props)
+        return (
+            <div className="header">
+                <div className="bounds">
+                    <h1 className="header--logo">MyAuth</h1>
+                    <nav>
+                        {authUser ?
+                            <React.Fragment>
+                                <span>Welcome, {authUser}!</span>
+                                <Link className="signout" to="/signout">Sign Out</Link>
+                            </React.Fragment>
+                            :
+                            <React.Fragment>
+                                <Link className="signup" to="/signup">Sign Up</Link>
+                                <Link className="signin" to="/signin">Sign In</Link>
+                            </React.Fragment>
+                        }
+                    </nav>
+                </div>
             </div>
-        </header>
-    )
-}
+        );
+    }
+};
+
+
+
+
