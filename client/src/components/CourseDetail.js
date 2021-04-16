37826fe394fc1d2a9c331f73b8a5e106c7a26c49
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-16 16:12:20 +0200

    Add DeleteHandler

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
+import {Link, useLocation } from "react-router-dom";
 
 import axios from "axios";
 
 import Course from "./Course";
 
-export default function CourseDetail() {
+export default function CourseDetail(props) {
 
     // Parse url parameter https://flaviocopes.com/how-to-get-last-item-path-javascript/
     const getLastItem = thePath => thePath.substring(thePath.lastIndexOf('/') + 1)
@@ -34,6 +34,17 @@ export default function CourseDetail() {
             .catch(error => console.log('Error fetching and parsing data', error))
             .finally(()=> setIsLoading(false))
     }, [urlParam])
+
+    function handleDelete(e) {
+        e.preventDefault();
+        const {context} = props;
+        context.data.deleteCourse(urlParam, context.authenticatedUser.emailAddress, context.authenticatedUser.password)
+            .then(() => props.history.push('/'))
+            .catch(err => {
+                console.log(err);
+                props.history.push('/error');
+            })
+    }
 
     return (
         <main>
