91d16c85ba7105699d75acf2e7289fa14aa653f9
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-16 21:47:38 +0200

    Fix css

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default () => (
-    <div className="bounds">
+    <div className="wrap">
         <h1>Not Found</h1>
         <p>Sorry! We couldn't find the page you're looking for.</p>
     </div>
