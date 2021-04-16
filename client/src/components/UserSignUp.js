f42bd7e6f4f1d890d2f8a7300bed77fe707acba4
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-16 23:25:06 +0200

    Remove unused Error component import (warning from React)

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Form from './Form';
-import Errors from "./Errors";
 
 export default class UserSignUp extends Component {
     state = {
