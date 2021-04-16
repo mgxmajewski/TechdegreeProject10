2b802f621dc0a5cb5074f6df1e36a4d98e66c9c2
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-16 17:11:54 +0200

    Refactor CreateCourse to use context for submitting course

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import ValidationError from "./ValidationError";
+import Errors from "./Errors";
 import axios from "axios";
 
 
@@ -15,27 +15,45 @@ export default function CreateCourse(props) {
         userId: context.authenticatedUser.id
     })
     const [response, setResponse] = useState([])
+    const [errors, setErrors] = useState([]);
 
     //https://rangle.io/blog/simplifying-controlled-inputs-with-hooks/
 
-    const submit = (e) => {
-        e.preventDefault();
-        axios.post(`http://localhost:5000/api/courses/`, {
-            title: course.title,
-            description: course.description,
-            estimatedTime: course.estimatedTime,
-            materialsNeeded: course.materialsNeeded,
-            userId: context.authenticatedUser.id
-        })
-            .then(response => setResponse(response.data))
-            .catch(error => console.log('Error fetching and parsing data', error))
-    }
+    // const submit = (e) => {
+    //     e.preventDefault();
+    //     axios.post(`http://localhost:5000/api/courses/`, {
+    //         title: course.title,
+    //         description: course.description,
+    //         estimatedTime: course.estimatedTime,
+    //         materialsNeeded: course.materialsNeeded,
+    //         userId: context.authenticatedUser.id
+    //     })
+    //         .then(response => setResponse(response.data))
+    //         .catch(error => console.log('Error fetching and parsing data', error))
+    // }
 
     const handle = (e) => {
         const newCourse = {... course}
         newCourse[e.target.id] = e.target.value
         setCourse(newCourse)
         console.log(newCourse)
+    }
+
+    function submit(e) {
+        e.preventDefault();
+        const {context} = props;
+        context.data.createCourse(course, context.authenticatedUser.emailAddress, context.authenticatedUser.password)
+            .then((err) => {
+                if (err.length) {
+                    setErrors(err)
+                } else {
+                    props.history.push('/');
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                props.history.push('/error');
+            })
     }
 
     console.log(response)
