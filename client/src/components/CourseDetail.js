d68fb128445c06bce8002b0217a19b4b22c537ff
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-16 16:11:47 +0200

    Add hook to keep email of author separate and get auth user email

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 import axios from "axios";
-import { useLocation } from "react-router-dom";
+
 import Course from "./Course";
 
 export default function CourseDetail() {
@@ -14,12 +14,23 @@ export default function CourseDetail() {
     // Add hooks to manage state of component
     const [course, setCourse] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const [authorEmail, setAuthorEmail] = useState('')
+
+    //Get authenticated user email via context
+    const {context} = props;
+    let authUserEmail = '';
+    if (context.authenticatedUser) {
+        authUserEmail = context.authenticatedUser.emailAddress
+    }
 
     // Fetch from API
     useEffect( () => {
         setIsLoading(true)
         axios.get(`http://localhost:5000/api/courses/${urlParam}`)
-            .then(course => setCourse(course.data))
+            .then(course => {
+                setCourse(course.data)
+                setAuthorEmail(course.data.User.emailAddress)
+            })
             .catch(error => console.log('Error fetching and parsing data', error))
             .finally(()=> setIsLoading(false))
     }, [urlParam])
