f37cae1c0f6117f731bbcc4affa9404dd85e72e9
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-17 03:36:36 +0200

    Fix style of NotFound Route

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => (
     <div className="wrap">
-        <h1>Not Found</h1>
+        <h2>Not Found</h2>
         <p>Sorry! We couldn't find the page you're looking for.</p>
     </div>
 );
